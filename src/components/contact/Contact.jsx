0187a7011df4a9e3cf95948744c551f459576a08
samhitaveluri <121224422+samhitaveluri@users.noreply.github.com>
2024-03-13 21:30:58 +0530

    Update Contact.jsx

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -31,10 +31,10 @@ const Contact = () => {
 
     emailjs
       .sendForm(
-        "service_94y20xo",
-        "template_v10u2oh",
+        "service_tcg9w2h",
+        "template_f08psgu",
         formRef.current,
-        "pX_2hasGmGcuvjXIW"
+        "r_9L5JFFpNpFBEqC4"
       )
       .then(
         (result) => {

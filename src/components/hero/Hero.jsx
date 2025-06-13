d5465818d8819f6fd28929062d32aab99a3eded2
samhitaveluri <121224422+samhitaveluri@users.noreply.github.com>
2025-06-13 13:46:48 +0530

    Update

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -49,7 +49,7 @@ const Hero = () => {
         >
           <motion.h2 variants={textVariants}>Samhita Veluri</motion.h2>
           <motion.h1 variants={textVariants}>
-            Web developer 
+            Software developer 
           </motion.h1>
           <motion.div variants={textVariants} className="buttons">
             <motion.button variants={textVariants}>
